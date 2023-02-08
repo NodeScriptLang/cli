58f4c2312de0d9e20d9fc7ea2a1d21901c6d8a74
Boris Okunskiy <boris@okunskiy.name>
2023-02-08 13:29:28 +0300

    chore: use @nodescript/bundler

--- src/main/managers/builder.ts
+++ src/main/managers/builder.ts
@@ -1,56 +1,18 @@
 import { createHash } from 'node:crypto';
 
+import * as bundler from '@nodescript/bundler';
 import { ModuleSpecSchema } from '@nodescript/core/schema';
 import { evalEsmModule } from '@nodescript/core/util';
-import { build } from 'esbuild';
 
 export class BuilderManager {
 
     async buildModuleFile(file: string) {
-        const computeCode = await this.compileCompute(file);
-        const moduleCode = await this.compileModuleJson(file);
+        const computeCode = await bundler.bundleModuleCompute(file);
+        const moduleCode = await bundler.bundleModuleJson(file);
         const { module } = await evalEsmModule(moduleCode);
         const moduleSpec = ModuleSpecSchema.decode(module);
         const computeHash = createHash('sha256').update(computeCode).digest('hex');
         return { computeCode, computeHash, moduleSpec };
-    }
-
-    private async compileCompute(file: string) {
-        const res = await build({
-            bundle: true,
-            stdin: {
-                resolveDir: process.cwd(),
-                loader: /\.ts/i.test(file) ? 'ts' : 'js',
-                contents: [
-                    `import { compute } from ${JSON.stringify('./' + file)};`,
-                    `export { compute };`,
-                ].join('\n'),
-            },
-            write: false,
-            minify: true,
-            keepNames: true,
-            format: 'esm',
-        });
-        return res.outputFiles[0].text;
-    }
-
-    private async compileModuleJson(file: string) {
-        const res = await build({
-            bundle: true,
-            stdin: {
-                resolveDir: process.cwd(),
-                loader: /\.ts/i.test(file) ? 'ts' : 'js',
-                contents: [
-                    `import { module } from ${JSON.stringify('./' + file)};`,
-                    `export { module };`,
-                ].join('\n'),
-            },
-            write: false,
-            minify: true,
-            keepNames: true,
-            format: 'esm',
-        });
-        return res.outputFiles[0].text;
     }
 
 }
